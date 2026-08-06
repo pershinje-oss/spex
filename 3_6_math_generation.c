8abf4cdafc712beab969caf6ede3864900395bd9
Evgeniy Pershin <pershinje453@gmail.com>
2026-08-06 12:36:30 +0300

    MATH_GENERATIONS_AND_CORRECT_BITS

---
+++ 3_6_math_generation.c
@@ -0,0 +1,99 @@
+#define _USE_MATH_DEFINES
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+
+int main(void){
+    
+    // int r_1 = rand();
+    // int r_2 = rand();
+
+    // printf("%d, %d, %d, %d, %d\n", r_1, r_2, rand(), rand(), rand());
+
+    // srand(10);
+    // printf("%d",  rand());
+
+    // time_t seconds = time(NULL);
+    // printf("Секунд с 1970 года: %ld\n", seconds);
+
+    // srand(time(NULL));
+    // printf("%d\n", rand());
+    
+    // srand(time(NULL));
+    // int range = 10;
+    // int r_1 = rand() % range; //0; range
+    // printf("%d\n", r_1);
+
+    // srand(time(NULL));
+    // int range = 10;
+    // int r_2 = rand() % range - 5;
+    // printf("%d", r_2);
+
+    // srand(time(NULL));
+    // int range = 10;
+    // int j = rand() % (max - min + 1) + min;
+    // rand() % (max - min + 1) + min; ---- это формула генерации рандомного числа из определенного диапозона
+    
+    // printf("log2(exp(1)) = %.0lf", log2(8));
+    // printf("число эйлера = %.3lf", exp(1));
+
+    // double gr = 40.0;
+    // double radian = gr * M_PI / 180.0;
+    // double result = sin(radian);
+    // printf("sin(40) = %f\ncos(radian) = %lf\ntan(radian) = %lf\n", result,cos(radian),tan(radian));
+
+    // int a,b,c,d = rand();
+    // printf("%d %d %d %d", a,b,c,d);
+
+    // int N;
+    // scanf("%d", &N);
+    // for (int i = 0; i < 7; i++){
+    //     printf(i < 6 ? "%d " : "%d" , rand() % (N + 1));
+    // }
+
+    // int a,b;
+    // scanf("%d %d", &a,&b);
+    // for (int i = 0; i < 10; i++){
+    //     printf("%d ", rand() % (b - a + 1) + a);
+    // }
+
+    // int a, b;
+    // scanf("%d %d", &a, &b); 
+    // for (int i = 0; i < 10; i++){
+    //     if(i > 0)printf(" ");
+    //     double ref = (rand() / (double)RAND_MAX) * (b - a) + a;
+    //     printf("%.2lf", ref);
+    // }
+
+    // int r1,r2,r3,r4,r5;
+    // srand(time(NULL));
+    // r1 = rand();
+    // r2 = rand();
+    // r3 = rand();
+    // r4 = rand();
+    // r5 = rand();
+
+    // unsigned short N;
+    // scanf("%hu", &N);
+    // printf("%hu", (unsigned short)ceil((double)(N * 7) / (double)100));
+
+    // unsigned short N;
+    // scanf("%hu", &N);
+    // printf("%hu", (unsigned short)round(N * 2.54));
+
+    // float gradus;
+    // scanf("%f", &gradus);
+    // double radian = gradus * M_PI / 180;
+    // printf("%.1lf", sin(radian));
+
+    // int a, b;
+    // scanf("%d, %d", &a, &b);
+    // double tag = (double)a / (double)b;
+    // double rad = atag(tag);
+    // printf("%.2lf %.2lf", tag, rad);
+
+    
+
+    return 0;
+}
